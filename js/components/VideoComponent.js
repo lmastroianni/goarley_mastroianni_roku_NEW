e0e9752d4ae00a353cccd05a181c171ed3d17d9f
Lucas <lcmhockey@hotmail.com>
2020-04-07 21:49:31 -0400

    video + thumbnails working

--- js/components/VideoComponent.js
+++ js/components/VideoComponent.js
@@ -2,18 +2,28 @@ export default {
     name: "TheVideoComponent",
 
     template: `
-    <div class="row">
-        <div class="col-12 order-2 order-md-1 col-md-3 media-container">
-            <h4 class="media-title">{{currentMediaDetails.movies_title}}</h4>
-            <p class="media-details" v-html="currentMediaDetails.movies_storyline"</p>
-            <span class="media-time">{{currentMediaDetails.movies_runtime}}</span>
-            <span class="media-year">{{currentMediaDetails.movies_year}}</span>
-        </div>
+    <section>
+        <div class="row">
+            <div class="col-12 order-2 order-md-1 col-md-3 media-container">
+                <h4 class="media-title">{{currentMediaDetails.movies_title}}</h4>
+                <p class="media-details" v-html="currentMediaDetails.movies_storyline"></p>
+                <span class="media-time">{{currentMediaDetails.movies_runtime}}</span>
+                <span class="media-year">{{currentMediaDetails.movies_year}}</span>
+            </div>
 
-        <div class="col-12 order-1 order-md-2 col-md-9 media-container">
-            <video autoplay controls muted :src="'video/' + currentMediaDetails.movies_trailer" class="fs-video"></video>
+            <div class="col-12 order-1 order-md-2 col-md-9 media-container">
+                <video autoplay controls muted :src="'video/' + currentMediaDetails.movies_trailer" class="fs-video"></video>
+            </div>
         </div>
-    </div>
+        <div class="row">
+         <div class="col-12 col-sm-9">
+            <div class="thumb-wrapper clearfix">
+                <img v-for="item in allRetrievedVideos" :src="'images/' + item.movies_cover" alt="media
+                thumb" @click="loadNewMovie(item)" class="img-thumbnail rounded float-left media-thumb">
+                </div>
+            </div>
+        </div>
+    </section>
     `,
 
     data: function () {
@@ -30,18 +40,34 @@ export default {
     methods: {
         retrieveVideoContent() {
             // fetch all the video content here (don't care about filtering, genre etc at this point)
-            debugger;
+            // debugger;
 
-            let url = `./admin/index.php?media=movies_tbl`;
-            //store a video
-            fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                this.allRetrievedVideos = data;
-                this.currentMediaDetails = data[0];
-            })
+            if (localStorage.getItem("cachedVideo")) {
+                this.allRetrievedVideos = JSON.parse(localStorage.getItem("cachedVideo"));
+
+                this.currentMediaDetails = this.allRetrievedVideos[0];
+
+            } else {
+
+                let url = `./admin/index.php?media=movies`;
+                //store a video
+                fetch(url)
+                .then(res => res.json())
+                .then(data => {
+                    localStorage.setItem("cachedVideo", JSON.stringify(data));
+
+                    this.allRetrievedVideos = data;
+                    this.currentMediaDetails = data[0];
+                })
+
+            }
+           
 
 
+        },
+
+        loadNewMovie(movie) {
+            this.currentMediaDetails = movie;
         }
     }
 }
